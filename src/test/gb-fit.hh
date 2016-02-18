f6a50d46dd8805d16db60d5f4d51b666604dda6c
Peter Salvi <vukung@yahoo.com>
2016-02-18 10:11:40 +0900

    Merge fix.

--- src/test/gb-fit.hh
+++ src/test/gb-fit.hh
@@ -9,5 +9,5 @@ Point2DVector parameterizePoints(const Surface &surf, const PointVector &points)
 SurfaceGeneralizedBezier fitWithOriginal(const SurfaceGeneralizedBezier &original,
                                          const PointVector &points,
                                          const Point2DVector &params,
-                                         double smoothing = 0);
+                                         double smoothing = 0, size_t fixed_rows = 2);
 DoubleVector deviationFromMesh(const SurfaceGeneralizedBezier &surf, const TriMesh &mesh);
